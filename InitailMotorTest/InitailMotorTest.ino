0b8c12672eec44a0b4f47550eb6900e3db135b6e
Vincent <33723845+VinStr@users.noreply.github.com>
2018-07-10 09:13:21 +0100

    reverse speed attempt 1

--- InitailMotorTest/InitailMotorTest.ino
+++ InitailMotorTest/InitailMotorTest.ino
@@ -1,7 +1,7 @@
-#define encoderA 3;
-#define encoderB 2;
-#define pwm 5;                      //Power of motor.
-#define dir 4;                       //Direction of the motor.
+#define encoderA 3
+#define encoderB 2
+#define pwm 5                      //Power of motor.
+#define dir 4                       //Direction of the motor.
 
 #define total 980                        //x4 pulses per rotation.
 #define motorSpeed 180                   //Change speed of the motor.
@@ -13,13 +13,23 @@ int NUM_KEYS = 5;
 long pulses = 0;                              //Output pulses.
 int deg = 0;
 
-double speedRPM = 0;
+double pv_speed = 0;
 unsigned long previousMillis = 0;        // will store last time LED was updated
 
 int m_direction = 0;
 int encoder = 0;
 
 double pwm_pulse = 0;
+
+double set_speed = 0;
+double e_speed = 0; //error of speed = set_speed - pv_speed
+double e_speed_pre = 0;  //last error of speed
+double e_speed_sum = 0;  //sum error of speed
+double kp = 1;
+double ki = 0;
+double kd = 0;
+
+#define sumLims 4000
 
 //const int analogIn = A0;
 //int mVperAmp = 100; // use 100 for 20A Module and 66 for 30A Module
@@ -47,8 +57,22 @@ void setup() {
 
 
 void loop(){
-  Serial.println(pulses);
+  Serial.println(pv_speed);
+  
+//  e_speed = set_speed - pv_speed;
+//  pwm_pulse = e_speed*kp + e_speed_sum*ki + (e_speed - e_speed_pre)*kd;
+//  e_speed_pre = e_speed;  //save last (previous) error
+//  e_speed_sum += e_speed; //sum of error
+//  if (e_speed_sum > sumLims) e_speed_sum = sumLims;
+//  if (e_speed_sum < -1*sumLims) e_speed_sum = -1*sumLims;
 
+  if(Serial.available()){
+    double in = (double)Serial.parseFloat();
+    if(in < 0) digitalWrite(dir, LOW);
+    else digitalWrite(dir, HIGH);
+    pwm_pulse = abs(in);
+  }
+  
   analogWrite(pwm,pwm_pulse); // minimum 11
   
 }
@@ -56,20 +80,9 @@ void loop(){
 void change(){
   unsigned long currentMillis = micros();
   double diff = (double)(currentMillis-previousMillis)/1000;
-  speedRPM = ((double)1/(double)98)/(diff/((double)1000*(double)60));
+  pv_speed = ((double)1/(double)98)/(diff/((double)1000*(double)60));
   previousMillis = currentMillis;
-
-        
-  e_speed = set_speed - pv_speed;
-  pwm_pulse = e_speed*kp + e_speed_sum*ki + (e_speed - e_speed_pre)*kd;
-  e_speed_pre = e_speed;  //save last (previous) error
-  e_speed_sum += e_speed; //sum of error
-  if (e_speed_sum >4000) e_speed_sum = 4000;
-  if (e_speed_sum <-4000) e_speed_sum = -4000;
-  
 }
-
-
 
 void A_CHANGE(){
   change();
